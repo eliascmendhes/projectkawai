133ec7b4f14d8e337432fc420d002549219a4383
Elias <eliascamilo70@outlook.com>
2024-11-17 00:10:20 -0300

    att and new features

---
+++ src/data/posts.ts
@@ -0,0 +1,15 @@
+export const posts = [
+  {
+    title: "Primeiro Post",
+    date: "2024-11-16",
+    preview: "Primeiras reflexões sobre este projeto e planos para o futuro.",
+    image: "https://99gifshop.neocities.org/items/4/mittel.gif",
+    slug: "primeiro-post",
+    content: `
+      Claro, preciso falar como isso aqui está funcionando. Espero levar este projeto por alguns anos, sem tempo de acabar. 
+      Só preciso melhorar: adicionar mais uns recursos aqui, outros ali. 
+      Quero comentar sobre minha vida, seja para algumas pessoas ou para mim mesmo. 
+      Bem, ainda há muito a ser feito.
+    `
+  }
+];
