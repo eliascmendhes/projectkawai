aef56247cb22a33e32ea29e323d21a2f889339e1
Elias <eliascamilo70@outlook.com>
2024-11-17 11:47:28 -0300

    form funcionando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import dotenv from 'dotenv';
 import vercel from '@astrojs/vercel/serverless';
-
+dotenv.config();
 export default defineConfig({
   integrations: [
     tailwind()
