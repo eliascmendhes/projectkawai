632547d15d7e515452d777a006a861cd48f25a99
Elias <eliascamilo70@outlook.com>
2024-11-17 23:11:40 -0300

    new gallery feature

---
+++ src/data/images.ts
@@ -0,0 +1,60 @@
+// data/images.ts
+
+interface Image {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+}
+
+export const images: Image[] = [
+  {
+    src: "/images/download.jpeg",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif01.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif02.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif03.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif04.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif05.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  {
+    src: "/images/gif06.gif",
+    alt: "",
+    title: "",
+    description: ""
+  },
+  
+  {
+    src: "/images/gif08.gif",
+    alt: "",
+    title: "",
+    description: ""
+  }
+];
