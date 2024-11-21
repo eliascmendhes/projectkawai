1a961d3f066c2d605423fe8ad286aa78526e91c5
Elias <eliascamilo70@outlook.com>
2024-11-21 01:23:15 -0300

    improve

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,60 +1,48 @@
 // data/images.ts
 
 interface Image {
-  src: string;
-  alt: string;
-  title: string;
-  description: string;
+  src: string
+  alt: string
+  title: string
+  description: string
 }
 
 export const images: Image[] = [
   {
-    src: "/images/download.jpeg",
-    alt: "teste",
-    title: "teste",
-    description: "teste teste teste "
+    src: '/images/images-gallery/image02.jpeg',
+    alt: 'teste',
+    title: 'teste',
+    description: 'teste teste teste ',
   },
   {
-    src: "/images/gif01.gif",
-    alt: "",
-    title: "",
-    description: ""
+    src: '/images/images-gallery/image04.jpeg',
+    alt: '',
+    title: '',
+    description: '',
   },
   {
-    src: "/images/gif02.gif",
-    alt: "",
-    title: "",
-    description: ""
+    src: '/images/images-gallery/image05.jpg',
+    alt: '',
+    title: '',
+    description: '',
   },
   {
-    src: "/images/gif03.gif",
-    alt: "",
-    title: "",
-    description: ""
+    src: '/images/images-gallery/imageo7.jpg',
+    alt: '',
+    title: '',
+    description: '',
   },
   {
-    src: "/images/gif04.gif",
-    alt: "",
-    title: "",
-    description: ""
+    src: '/images/images-gallery/image08.jpg',
+    alt: '',
+    title: '',
+    description: '',
   },
   {
-    src: "/images/gif05.gif",
-    alt: "",
-    title: "",
-    description: ""
+    src: '/images/images-gallery/image14.jpg',
+    alt: '',
+    title: '',
+    description: '',
   },
-  {
-    src: "/images/gif06.gif",
-    alt: "",
-    title: "",
-    description: ""
-  },
-  
-  {
-    src: "/images/gif08.gif",
-    alt: "",
-    title: "",
-    description: ""
-  }
-];
+
+]
